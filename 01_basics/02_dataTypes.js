ecef303c77372bbef03ba01424673abda937deed
sheikh757 <k001m.abdullah@gmail.com>
2024-12-22 20:01:41 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"Use Strict"; // treat all js code as newer version
+
+// alert(3 +3 ) we are using node js not browser
+console.log(3+
+    3) // do not do this as this fucks up code readability
+
+
+console.log("Sheikh")
+
+let name = "sheikh"
+let age = 22
+let isLoggedIn= false
+
+// number=>2 the power of 53
+// bigint
+// string=>""
+// boolean=> true/false
+// null => is a standalone value
+// undefined => 
+// symbol => unique
+
+//object
+
+console.log(typeof "Sheikh")
+//typeoff to tell datatype of variable
+
+console.log(typeof undefined)
+// output is undeifined
+
+console.log(typeof null)
+// output is object
